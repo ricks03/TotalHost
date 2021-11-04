0d4be28ea51d28aedb6cee539efd25f654ee8c83
Rick Steeves <github@corwyn.net>
2021-11-04 00:34:07 -0400

    Block 9
    Reads Block 9

--- scripts/StarsHash.pl
+++ scripts/StarsHash.pl
@@ -1,11 +1,10 @@
 # StarsHash.pl
 # Deals with Block 9 File Hash Block
-# NOT WORKING
 #
 # Rick Steeves
 # starsah@corwyn.net
 # Version History
-# 191119
+# 191119, 211102
 #
 #     Copyright (C) 2019 Rick Steeves
 # 
@@ -24,24 +23,15 @@
 #     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-# Displays the bytes of a Stars! Block
+# Displays the contents of Block 9
 # Copy Protection Activates When Editing an Allies Turn File
 # https://wiki.starsautohost.org/wiki/Known_Bugs
 
-# Factors in 
-#// Fills in the vrgbEnvCur buffer with the following:
 #// szWork[0] - szWork[3] : Label C:
 #// szWork[4] - szWork[5] : C: date/time of volume
 #// szWork[6] - szWork[8] : Label D:
 #// szWork[9]             : D: date/time of volume
 #// szWork[10]            : C: and D: drive size in 100's of MB
-#// Returns the length of vrgbEnvCur data (usually 11)
-
-# GlobalSettings=IUK30dFAN9eY1pKABAL3tVcWUpnp
-# is for SAH62J1E
-
-# GlobalSettings=DVK31UFA29lCFpKAAwZb1VcW1onp
-# is for E2WAENCB
 
 # Example Usage: StarsHash.pl c:\stars\game.x1
 #
@@ -55,24 +45,22 @@ use StarsBlock; # A Perl Module from TotalHost
 my $debug = 1; # Enable better debugging output. Bigger the better
 
 ##########  
-my $inBlock = '';
-my $inBin = 0; 
-my $inName;
 my $filename; 
-$inName = $ARGV[0]; # input file
-$inBlock = $ARGV[1]; # Desired block Type
-$inBin = $ARGV[2]; # Desired block Type
-unless ($inBlock) { $inBlock = -1;}
-$filename = $inName;
+$inFile1 = $ARGV[0]; # input file 1
+$inFile2 = $ARGV[1]; # input file 2
+$filename = $inFile1;
 
-if (!($inName)) { 
-  print "\n\nDisplays the hash information in Stars! blocks.\n";
+if (!($inFile1)) { 
+  print "\n\nDisplays the file hash block information in Stars! .x file.\n";
   exit;
 }
 
 #Validate directory or file 
-unless (-e $inName ) { 
-  print "File: $inName does not exist!\n"; exit; 
+unless (-e $inFile1 ) { 
+  print "File: $inFile1 does not exist!\n"; exit; 
+}
+unless (-e $inFile2 && $inFile2) { 
+  print "File: $inFile2 does not exist!\n"; exit; 
 }
 
 my ($basefile, $dir, $ext);
@@ -116,38 +104,28 @@ sub decryptBlock {
     @data =   @fileBytes[$offset+2 .. $offset+(2+$size)-1]; # The non-header portion of the block
     @block =  @fileBytes[$offset .. $offset+(2+$size)-1]; # The entire block in question
 
-    #if ($debug) { print "\nBLOCK typeId: $typeId, Offset: $offset, Size: $size\n"; }
-    if ($debug > 1) { print "BLOCK RAW: Size " . @block . ":\n" . join ("", @block), "\n"; }
     # FileHeaderBlock, never encrypted
     if ($typeId == 8) {  # File Header Block
       # Convert the nonencrypted Block 8 data
       my ($unshiftedData) = &unshiftBytes(\@data); 
       my @unshiftedData = @{ $unshiftedData };
-      if ($debug) { print "BLOCK:$typeId,Offset:$offset,Bytes:$size\t"; }
-      if ($debug) { print "DATA UNSHIFTED:" . join (" ", @unshiftedData), "\n"; }
+      if ($debug>1) { print "BLOCK:$typeId,Offset:$offset,Bytes:$size\t"; }
+      if ($debug>1) { print "DATA UNSHIFTED:" . join (" ", @unshiftedData), "\n"; }
 
       # We always have this data before getting to block 6, because block 8 is first
       # If there are two (or more) block 8s, the seeds reset for each block 8
       ( $binSeed, $fShareware, $Player, $turn, $lidGame, $Magic, $fMulti) = &getFileHeaderBlock(\@block);
       ( $seedA, $seedB) = &initDecryption ($binSeed, $fShareware, $Player, $turn, $lidGame);
-      my $gameId = &read32(\@unshiftedData, 4); 
-      print "gameId: $gameId  lidGame: $lidGame\n";
-    } elsif ($typeId == 0) {
-      my ($unshiftedData) = &unshiftBytes(\@data); 
-      my @unshiftedData = @{ $unshiftedData };
-      $debug = 1;
-      if ($debug) { print "BLOCK:$typeId,Offset:$offset,Bytes:$size\t"; }
-      if ($debug) { print "DATA DECRYPTED:" . join (" ", @decryptedData), "\n"; }
-
     } else {
       # Everything else needs to be decrypted
       ($decryptedData, $seedA, $seedB, $padding) = &decryptBytes(\@data, $seedA, $seedB); 
       @decryptedData = @{ $decryptedData };
-      $debug = 1;
-      if ($debug) { print "BLOCK:$typeId,Offset:$offset,Bytes:$size\t"; }
-      if ($debug) { print "DATA DECRYPTED:" . join (" ", @decryptedData), "\n"; }
+      if ($debug > 1) { print "BLOCK:$typeId,Offset:$offset,Bytes:$size\t"; }
+      if ($debug > 1) { print "DATA DECRYPTED:" . join (" ", @decryptedData), "\n"; }
       # WHERE THE MAGIC HAPPENS
       if ($typeId == 9) {
+        if ($debug) { print "BLOCK:$typeId,Offset:$offset,Bytes:$size\t"; }
+        if ($debug) { print "DATA DECRYPTED:" . join (" ", @decryptedData), "\n"; }
         &processData(\@decryptedData,$typeId,$offset,$size);
       }
       # END OF MAGIC
@@ -161,10 +139,18 @@ sub processData {
   # Display the byte information
   my ($decryptedData,$typeId,$offset,$size)  = @_;
   my @decryptedData = @{ $decryptedData };
-  my $ordersSum = &read16(\@decryptedData, 0); print "OrdersSum: $ordersSum\n";
-
-#The remaining 15 bytes are Machine and Serial no hashes according to PaulCr.
-  
-  die;
+  my $byte0 =  &read16(\@decryptedData, 0);  # unknown
+  my $serial = &read32(\@decryptedData, 2);  # serial number, blocks 2-5
+  print "Serial (Hash): $serial\n";
+  my $c_label = &read32(\@decryptedData, 6);  # C drive volume label, blocks 6-9
+  print "C: Label (Hash): $c_label\n";
+  my $c_date = &read16(\@decryptedData, 10); # blocks 10-11. Probably C volume date
+  print "C: Date (Hash): $c_date\n";
+  my $d_label = &read16(\@decryptedData, 12);  # D drive volume label, blocks 12-13
+  print "D: Label (Hash): $d_label\n";
+  my $d_date = &read16(\@decryptedData, 14);  # probably D volume date
+  print "D: Date (Hash): $d_date\n";
+  my $drive_size = &read8(\@decryptedData,16); # C: and D: drive size in 100's of MB
+  print "Drive Size (Hash): $drive_size\n";
 }
 
