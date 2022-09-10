540e18f6a31ba5950b7c67867b506344eb34377d
Pascal Dennerly <dnnrly@gmail.com>
2022-09-10 23:11:57 +0100

    Add platform info to health

--- scripts/health.pl
+++ scripts/health.pl
@@ -6,8 +6,9 @@ print "<p>healthy</p>\n";
 print "<p>checking local package import</p>\n";
 use StarsConfig;
 
-print "<p>Environment variables:</p>\n";
+print "<p>Platform: $^O</p>\n";
 
+print "<p>Environment variables:</p>\n";
 print "<table>\n";
 print "<tr><th>Key</th><th>Value</th></tr>\n";
 foreach $key (sort keys(%ENV)) {
