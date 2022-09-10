0fca2a18011b12aed6a34698bb7f4cb0f1114a42
Pascal Dennerly <dnnrly@gmail.com>
2022-09-10 22:36:07 +0100

    Make details in health page easier to read

--- scripts/health.pl
+++ scripts/health.pl
@@ -1,15 +1,16 @@
 #!/usr/bin/env perl
 
 print "Content-Type: text/html\n\n";
-print '<p>healthy</p>';
+print "<p>healthy</p>\n";
 
-print '<p>checking local package import</p>';
+print "<p>checking local package import</p>\n";
 use StarsConfig;
 
-print "<p>Environment variables:</p>";
-print "<pre>\n";
+print "<p>Environment variables:</p>\n";
 
+print "<table>\n";
+print "<tr><th>Key</th><th>Value</th></tr>\n";
 foreach $key (sort keys(%ENV)) {
-  print "$key = $ENV{$key}<p>";
+  print "<tr><td><pre>$key</pre></td><td><pre>$ENV{$key}</td></pre></tr>\n";
 }
-print "</pre>\n";
+print "</table>\n";
