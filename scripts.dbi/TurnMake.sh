b11b1fe2909eb8cfc503cef6e3604b224f83e10c
Rick Steeves <github@corwyn.net>
2024-11-06 02:15:36 -0500

    moving to utils

--- scripts.dbi/TurnMake.sh
+++
@@ -1,8 +0,0 @@
-cd /var/www/totalhost/scripts
-export DISPLAY=":99"
-export PERL5LIB=/var/www/totalhost/scripts:$PERL5LIB
-export PATH="$PATH:/var/www/totalhost/scripts"
-#export WINEDLLOVERRIDES="winex11.drv=b" # Disable wine clipboard. I don't seem to need this with xclip installed.
-#export WINEDLLOVERRIDES="mscoree,mshtml=" #                      . I don't seem to need this with xclip installed.
-#xhost +SI:localuser:www-data
-sudo -u www-data PERL5LIB=/var/www/totalhost/scripts perl /var/www/totalhost/scripts/TurnMake.pl
